af3750b9083e9bd89d2ef29fe3a1d1444e72c38f
Filip Lundgren <filip@sceneri.com>
2025-04-17 21:47:33 -0700

    Move vectorization support detection to C++

--- Public/Common/Common.h
+++ Public/Common/Common.h
@@ -3,3 +3,4 @@
 #include <Common/Platform/ConfigureCompiler.h>
 #include <Common/Platform/ConfigureArchitecture.h>
 #include <Common/Platform/ConfigurePlatform.h>
+#include <Common/Platform/ConfigureVectorization.h>
